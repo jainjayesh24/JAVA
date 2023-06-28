a26c34e2dcd47113072a2eec4d5f67dd8ed0be5c
JAYESH JAIN <83653797+TECHNOS12-TECH@users.noreply.github.com>
2023-06-28 20:36:50 +0530

    Update new.java

--- new.java
+++ new.java
@@ -16,3 +16,43 @@ public class Main {
 
     }
 }
+
+
+
+// CGPA Calculator
+package com.company;
+
+// Java.util module for taking input from keyboard
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("CGPA Maker");
+        Scanner SC = new Scanner(System.in);
+        System.out.println("Enter marks of first: ");
+        int marks1 = SC.nextInt();
+        System.out.println("Enter marks of Second: ");
+        int marks2 = SC.nextInt();
+        System.out.println("Enter marks of third: ");
+        int marks3 = SC.nextInt();
+        int CGPA = (marks1+marks2+marks3)/30;
+        System.out.println("The required CGPA is:  "+CGPA);
+
+    }
+}
+
+//Important Points
+// Pascal case Naming convention for classes: -->>> AddTwoNumbers
+// Camel case Naming convention for functions:-->>> addTwoNumbers
+// Whitespaces are not allowed in classes and FUNCTIONS(Identifiers)
+// sout is the keyword used for opertor printing in java
+// Packages--->Classes--->Functions
+// Declaration of variable
+// data_type variable_name = Value it stores
+// int number = 8
+//Variable declaration -->> Keyword(Not possible)
+//Cannot start with digit, white space is not allowed
+// contain alphabets, _ , $
+// taking input from java
+
