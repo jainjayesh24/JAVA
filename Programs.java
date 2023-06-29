6f71bcba13d052030f3f5c8c3f29911584f2c65f
JAYESH JAIN <83653797+TECHNOS12-TECH@users.noreply.github.com>
2023-06-29 20:21:31 +0530

    Update Programs.java

--- Programs.java
+++ Programs.java
@@ -23,3 +23,35 @@ public class Main {
 
     }
 }
+
+
+
+package com.company;
+
+// Java.util module for taking input from keyboard
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+	// write your code here
+        //Write a Program to increment value of number by 1 if the number is 1 and decrease by 1 if number is odd
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int a = sc.nextInt();
+        if (a%2==0) {
+            a++;
+            System.out.println("The number is even and the final value is: ");
+            System.out.println(a);
+        }
+        else
+            --a;
+            System.out.println("The number is odd and the final value is: ");
+            System.out.println(a);
+        System.out.println(6*5-34/2);
+        System.out.println((6*5-34)/2);
+        System.out.println(6*(5-34/2));
+    }
+}
+
+
+
