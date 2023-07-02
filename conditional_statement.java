16b87a518aa47dae7c7fba006500ee4323654f96
JAYESH JAIN <83653797+TECHNOS12-TECH@users.noreply.github.com>
2023-07-02 09:18:56 +0530

    Update conditional_statement.java

--- conditional_statement.java
+++ conditional_statement.java
@@ -23,6 +23,33 @@ public class Main {
     }
 }
 
+// NO days in month
+public class Main {
+    public static void main(String[] args) {
+        Scanner SC = new Scanner(System.in);
+        System.out.println("Enter Month(Number): ");
+        int a = SC.nextInt();
+        System.out.println("Enter Year: ");
+        int b = SC.nextInt();
+
+        //int intArray31[] = {1,3,5,7,8,10,12};
+        //int intArray30[] = {4,6,9,11};
+        if (a==2 && b%4==0){
+            System.out.println("29 Days");
+        }
+        else if(a==2 && b!=0){
+            System.out.println("28 Days");
+        }
+        else if(a==1 || a==3 || a==5 || a==7 || a==8 || a==10 || a==12){
+            System.out.println("31 days");
+        }else{
+            System.out.println("30 days");
+        }
+
+
+    }
+}
+
 
 // To find greatest of three number
 Scanner SC = new Scanner(System.in);
